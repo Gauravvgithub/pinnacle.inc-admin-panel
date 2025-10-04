4171875b1ff7c1a8e26c95ce21cd3ca131fca19f
gauravvgithub <gauravraj7011@gmail.com>
2025-10-04 11:23:17 +0530

    backend url added in .env

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const instance = axios.create({
   
   
-  baseURL: "https://pinnacle-inc-backend.vercel.app/api",
+  baseURL: `${import.meta.env.VITE_API_URL}`,
   withCredentials: true,
 });
 
